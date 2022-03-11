b4bb074f68c61e932ea60f454c082655caba9ea0
fescarvalho <fernando_scarvalho@hotmail.com>
2022-03-11 19:25:56 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,66 @@
+const controles = document.getElementById("controles");
+const cssText = document.querySelector(".css");
+const btn = document.querySelector(".btn");
+
+const handleStyle = {
+  element: btn,
+  backgroundColor(value) {
+    this.element.style.backgroundColor = value;
+  },
+  height(value) {
+    this.element.style.height = value + "px";
+  },
+  color(value) {
+    this.element.style.color = value;
+  },
+  width(value) {
+    this.element.style.width = value + "px";
+  },
+  border(value) {
+    this.element.style.border = value;
+  },
+  borderRadius(value) {
+    this.element.style.borderRadius = value + "px";
+  },
+  fontSize(value) {
+    this.element.style.fontSize = value + "rem";
+  },
+  texto(value) {
+    this.element.innerText = value;
+  },
+  fontFamily(value) {
+    this.element.style.fontFamily = value;
+  },
+};
+
+controles.addEventListener("change", handleChange);
+
+function handleChange(event) {
+  const name = event.target.name;
+  const value = event.target.value;
+
+  handleStyle[name](value);
+  saveValues(name, value);
+  showCss();
+}
+
+function saveValues(name, value) {
+  localStorage[name] = value;
+}
+
+function setValues() {
+  const properties = Object.keys(localStorage);
+
+  properties.forEach((propertie) => {
+    handleStyle[propertie](localStorage[propertie]);
+    controles.elements[propertie].value = localStorage[propertie];
+  });
+
+  showCss();
+}
+setValues();
+
+function showCss() {
+  cssText.innerHTML =
+    "<span>" + btn.style.cssText.split("; ").join(";</span><span>");
+}
